318580a350ed56cfa458cd98b302ceb6b4365529
Michael Amos <michael@mystenlabs.com>
2023-05-08 09:55:47 -0700

    explorer - add detail to tx view

--- apps/explorer/src/pages/transaction-result/TransactionView.tsx
+++ apps/explorer/src/pages/transaction-result/TransactionView.tsx
@@ -35,32 +35,7 @@ export function TransactionView({
 }) {
     const isMediumOrAbove = useBreakpoint('md');
 
-    // const txKindData = formatByTransactionKind(txKindName, txnDetails, sender);
-    // const txEventData = transaction.events?.map(eventToDisplay);
-
-    // MUSTFIX(chris): re-enable event display
-    // let eventTitles: [string, string][] = [];
-    // const txEventDisplay = txEventData?.map((ed, index) => {
-    //     if (!ed) return <div />;
-
-    //     let key = ed.top.title + index;
-    //     eventTitles.push([ed.top.title, key]);
-    //     return (
-    //         <div className={styles.txgridcomponent} key={key}>
-    //             <ItemView data={ed.top as TxItemView} />
-    //             {ed.fields && <ItemView data={ed.fields as TxItemView} />}
-    //         </div>
-    //     );
-    // });
-
-    // let eventTitlesDisplay = eventTitles.map(([title, key]) => (
-    //     <div key={key} className={styles.eventtitle}>
-    //         {title}
-    //     </div>
-    // ));
-
-    // MUSTFIX(chris): re-enable event display
-    // const hasEvents = txEventData && txEventData.length > 0;
+    // todo: reimplement events tab
     const hasEvents = false;
 
     const txError = getExecutionStatusError(transaction);
@@ -74,7 +49,7 @@ export function TransactionView({
 
     const leftPane = {
         panel: (
-            <div className="h-full overflow-y-scroll rounded-2xl border border-transparent bg-gray-40 p-6 md:h-screen md:p-10">
+            <div className="h-full overflow-y-scroll rounded-2xl border border-transparent bg-gray-40 p-6 md:h-full md:max-h-screen md:p-10">
                 <TabGroup size="lg">
                     <TabList>
                         <Tab>Summary</Tab>
@@ -87,18 +62,6 @@ export function TransactionView({
                                 <TransactionSummary transaction={transaction} />
                             </div>
                         </TabPanel>
-                        {/* {hasEvents && (
-                        <TabPanel>
-                            <div className={styles.txevents}>
-                                <div className={styles.txeventsleft}>
-                                    {eventTitlesDisplay}
-                                </div>
-                                <div className={styles.txeventsright}>
-                                    {txEventDisplay}
-                                </div>
-                            </div>
-                        </TabPanel>
-                    )} */}
                         <TabPanel>
                             <Signatures transaction={transaction} />
                         </TabPanel>
